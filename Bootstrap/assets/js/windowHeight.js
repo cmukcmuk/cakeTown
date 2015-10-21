1bbf5bab4ca843da8cdcb1e89dba1c719b20ce05
pacificano <tris.pacificano@gmail.com>
2015-10-21 12:41:07 +1100

    the window height changer for divs

---
+++ Bootstrap/assets/js/windowHeight.js
@@ -0,0 +1,24 @@
+$(function () {
+
+        var $windowHeight = $(window).height();
+        var $headerHeight = $("header").height();
+
+        function windowHeight () {
+
+            var $windowWidth = $(window).width();
+
+            if ($windowWidth > 768) {
+                $("header").css("min-height", $windowHeight);
+            } else {
+                $("header").css("min-height", $headerHeight); // can change this to be nothing. wait, do i even need to do this?
+            };
+
+        };
+
+        $(window).resize(function() {
+            windowHeight();
+        });
+
+        windowHeight();
+
+});
